c22a3e41643a9a0c51b571175c68e0552b36fff4
Chris Bosco <chris@masterstreet.com>
2014-05-23 10:10:30 -0400

    Minification: broccoli-env and broccoli-uglify-js added

--- Brocfile.js
+++ Brocfile.js
@@ -1,8 +1,17 @@
 var mergeTrees = require('broccoli-merge-trees');
 var findBowerTrees = require('broccoli-bower');
+var env = require('broccoli-env').getEnv();
+var uglifyJavaScript = require('broccoli-uglify-js');
 
 var publicFiles = 'public';
 var appJs = 'app';
 var appCss = 'styles';
+
+if (env === 'production') {
+  appJs = uglifyJavaScript(appJs, {
+    // mangle: false,
+    // compress: false
+  });
+}
 
 module.exports = mergeTrees([appJs, appCss, publicFiles].concat(findBowerTrees()));
